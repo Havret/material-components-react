6153ceba8c0369236200cf6f31b4db8337b31bca
Krzysztof <h4vret@gmail.com>
2018-01-20 22:12:00 +0100

    Filter out not documented properties from docs (#37)

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'Material Components for React',
     components: 'src/**/*.tsx',
     ignore: ['**/*.test.tsx', '**/*.test.ts'],
-    propsParser: require('react-docgen-typescript').withDefaultConfig().parse,
+    propsParser: require('react-docgen-typescript').withDefaultConfig({propFilter: {skipPropsWithoutDoc: true}}).parse,
     template: './docs/index.html',
     require: [
         './docs/styles.css',
