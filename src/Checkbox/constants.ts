57831ac24c5833007685b0c8e9b0bd7aaab5c82c
Havret <h4vret@gmail.com>
2018-02-24 16:18:26 +0100

    Apply breaking change for Checkbox

--- src/Checkbox/constants.ts
+++ src/Checkbox/constants.ts
@@ -4,7 +4,7 @@ export const NATIVE_CONTROL = 'mdc-checkbox__native-control';
 export const DISABLED = 'mdc-checkbox--disabled';
 export const BACKGROUND = 'mdc-checkbox__background';
 export const CHECKMARK = 'mdc-checkbox__checkmark';
-export const CHECKMARK_PATH = 'mdc-checkbox__checkmark__path';
+export const CHECKMARK_PATH = 'mdc-checkbox__checkmark-path';
 export const MIXEDMARK = 'mdc-checkbox__mixedmark';
 
 export const ANIM_UNCHECKED_CHECKED = 'mdc-checkbox--anim-unchecked-checked';
@@ -14,7 +14,7 @@ export const ANIM_CHECKED_INDETERMINATE = 'mdc-checkbox--anim-checked-indetermin
 export const ANIM_INDETERMINATE_CHECKED = 'mdc-checkbox--anim-indeterminate-checked';
 export const ANIM_INDETERMINATE_UNCHECKED = 'mdc-checkbox--anim-indeterminate-unchecked';
 
-export const ANIM_END_LATCH_MS = 100;
+export const ANIM_END_LATCH_MS = 250;
 
 export enum CheckState {
     checked,
