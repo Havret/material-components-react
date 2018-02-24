97735dbe280ec68e86566eb30a8bfda4a3b3c1d3
Havret <h4vret@gmail.com>
2018-02-24 20:21:11 +0100

    Implement Elevation Component

--- src/Elevation/Elevation.tsx
+++ src/Elevation/Elevation.tsx
@@ -1,29 +1,34 @@
+import * as cx from 'classnames';
 import * as React from 'react';
-import { HTMLProps, ReactNode } from 'react';
+import { ReactNode } from 'react';
+import { ELEVATION, ELEVATION_TRANSITION } from './constants';
 
-interface ElevationProps extends HTMLProps<HTMLInputElement> {
+interface ElevationProps {
+    /**
+     * Addition CSS classes that will be added to the root element.
+     */
+    className?: string;
     /**
      * The content of the Elevation.
      */
     children?: ReactNode;
     /**
-     * Addition CSS classes that will be added to the root element.
+     *    Sets the elevation to the (N)dp, where 0 <= N <= 24.
      */
-    z: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24;
+    z: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24;
     /**
-     * Allows for smooth transitions between elevations when the z value changes.
+     *    Applies the correct css rules to transition an element between elevations.
      */
     transition?: boolean;
 }
 
 class Elevation extends React.Component<ElevationProps, {}> {
     render() {
-        const {className} = this.props;
+        const {className, children, transition, z, ...rest} = this.props;
+        const classNames = cx(`${ELEVATION}${z}`, {[ELEVATION_TRANSITION]: transition}, className);
 
-        return (
-            <div></div>
-        );
+        return (<div {...rest} className={classNames}>{children}</div>);
     }
 }
 
-export { Elevation as default, Elevation };
+export { Elevation as default, Elevation, ElevationProps };
---
+++ src/Elevation/constants.ts
@@ -0,0 +1,2 @@
+export const ELEVATION = 'mdc-elevation--z';
+export const ELEVATION_TRANSITION = 'mdc-elevation-transition';
